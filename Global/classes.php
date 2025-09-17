6d3e78b2891509aed7487324a33e9e2dc74266e9
Dezrao1 <168411191+Dezrao1@users.noreply.github.com>
2025-09-17 12:40:38 +0300

    Updated classes.php

--- Global/classes.php
+++ Global/classes.php
@@ -1,11 +1,12 @@
 <?php
-// Create a class for HelloWorld
+
 class classes {
     public function greet() {
         return "<h1>Hello, ICS C Community!</h1>";
     }
 
     public function today() {
-        return "<p>Today is " . date("l") . "</p>";
+        return "<p>Today is " . date("l, F j, Y") . "</p>";
     }
 }
+?>
